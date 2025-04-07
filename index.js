15ea98f379c8452edbf25ecc30146757034cef95
Victor Kimathi <vkimathi97@gmail.com>
2025-04-08 01:06:25 +0300

    events-acting-on-events-lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+const dodger = document.getElementById("dodger")
+const moveDodgerLeft = () => {
+  const leftNumbers = dodger.style.left.replace("px", "")
+  const left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+
+}
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+  }
+  })
+  
+
+
+const moveDodgerRight = () => {
+  const leftNumbers = dodger.style.left.replace("px", "")
+  const left = parseInt(leftNumbers, 10)
+
+  if (left <= 179) {
+    dodger.style.left = `${left + 1}px`
+  }
+
+}
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowRight") {
+      moveDodgerLeft();
+  }
+  })
